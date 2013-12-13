a16e956df7c118a2b2dbb5e7a09be8689f0fc1b1
Nathan Evans <nathan.evans@pebblecode.com>
2013-12-13 11:20:54 +0000

    stuff

---
+++ Giftstarter/Models/AddWishlistItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Giftstarter.Models
+{
+    public class AddWishlistItem
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public decimal Price { get; set; }
+    }
+}
